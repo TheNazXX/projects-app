c18cd474dd2bddd24a72973040e91f619fbfdf71
Nazar <nz.shevchuk.2002@gmail.com>
2023-03-11 17:02:24 +0200

    axios + cemu-category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { GetStaticProps } from 'next';
 import { Htag, Button, P, Tag, Rating, withLayout } from '@/components';
 import { useState } from 'react';
+import axios from 'axios';
+import { MenuItem } from '@/interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({menu}: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4)
   return (
     <>
@@ -17,8 +20,30 @@ function Home(): JSX.Element {
       <Tag size='s' color='green'>Green tag</Tag>
       <Tag size='s' color='primary'>Primary tag</Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating}></Rating>
+      <ul>
+        {
+          menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))
+        }
+      </ul>
     </>
   );
 };
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN, + '/api/top-page/find',
+  {firstCategory});
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown>{
+  menu: MenuItem[],
+  firstCategory: number;
+}
