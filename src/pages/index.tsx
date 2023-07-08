d351c157e83b60f5cf2f8a4f6fba3f2d94a52690
Nazar <nz.shevchuk.2002@gmail.com>
2023-07-08 15:12:17 +0300

    debug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
-import { Htag } from '@/components';
-import { Button } from '@/components';
-import { Inter } from 'next/font/google';
-
 export default function Home(): JSX.Element {
+  const a = '1'
   return (
     <>
-      <Htag tag='h1'>Text</Htag>
-      <Button appearance='ghost' arrow='right'>Узнать подробнее</Button>
-      <Button appearance='primary'>Узнать подробнее</Button>
+      App {a}
     </>
   );
 };
